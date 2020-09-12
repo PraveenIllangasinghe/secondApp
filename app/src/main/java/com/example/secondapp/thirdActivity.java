74005ae342b9b05b1909da596d22d031d81bf5e5
PraveenIllangasinghe <praveenillangasinghe99@gmail.com>
2020-09-12 16:31:01 +0530

    final test part 2

---
+++ app/src/main/java/com/example/secondapp/thirdActivity.java
@@ -0,0 +1,17 @@
+package com.example.secondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thirdActivity extends AppCompatActivity {
+
+    private int tot = 10;
+    private boolean s1 = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
