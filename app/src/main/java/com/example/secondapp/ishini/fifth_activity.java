11e0f2cfc3ac30b3ca8c928d59ebde86c59c264e
Ishini Halgaswatta <ishini.hal@gmail.com>
2020-09-16 16:28:19 +0530

    changed by ishini

---
+++ app/src/main/java/com/example/secondapp/ishini/fifth_activity.java
@@ -0,0 +1,17 @@
+package com.example.secondapp.ishini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.secondapp.R;
+
+public class fifth_activity extends AppCompatActivity {
+    private int ishini = 8;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth_activity);
+    }
+}
