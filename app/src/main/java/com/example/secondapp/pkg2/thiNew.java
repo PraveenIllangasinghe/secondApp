94168b314c534ba6273ee72d933750d88eca86c2
PraveenIllangasinghe <praveenillangasinghe99@gmail.com>
2020-09-17 14:41:49 +0530

    thi inte

---
+++ app/src/main/java/com/example/secondapp/pkg2/thiNew.java
@@ -0,0 +1,16 @@
+package com.example.secondapp.pkg2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.secondapp.R;
+
+public class thiNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thi_new);
+    }
+}
